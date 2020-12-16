87337f0ad7cd9c6d85493db1184e64cb0e264efa
Ignacio Paradisi <ignacioparadisi@gmail.com>
2020-12-16 14:54:22 -0400

    Initial Commit

---
+++ Shared/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Shared
+//
+//  Created by Ignacio Paradisi on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
